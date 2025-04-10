3d7ec45cc053b9119987d49110b93aad1a5b7013
leyanadabare <leyanadabare1@gmail.com>
2025-04-10 17:15:57 +0530

    feat: implemented the Parser class

--- src/main/java/maxflow/Parser.java
+++ src/main/java/maxflow/Parser.java
@@ -1,4 +1,22 @@
 package maxflow;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Parser {
+    public static Graph parseFile(String path) throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File(path));
+        int V = scanner.nextInt();
+        Graph graph = new Graph(V);
+
+        while (scanner.hasNextInt()){
+            int from = scanner.nextInt();
+            int to = scanner.nextInt();
+            int cap = scanner.nextInt();
+            graph.addEdge(new Edge(from, to, cap));
+        }
+
+        scanner.close();
+        return graph;
+    }
 }
