7ca17cd6dc0b9864847992d0b03917367bd0b2c7
leyanadabare <leyanadabare1@gmail.com>
2025-04-10 22:28:01 +0530

    feat: implemented the Main class

--- src/main/java/maxflow/Main.java
+++ src/main/java/maxflow/Main.java
@@ -1,17 +1,13 @@
 package maxflow;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            Graph graph = Parser.parseFile("src/main/resources/benchmark1.txt");
+            MaxFlowSolver solver = new MaxFlowSolver(graph, 0, graph.V() - 1);
+            System.out.println("Max flow: " + solver.getMaxFlow());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
