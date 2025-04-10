3ec507a393a0662d11be18f8226d208bd400daf4
leyanadabare <leyanadabare1@gmail.com>
2025-04-10 22:35:14 +0530

    fix: corrected file path

--- src/main/java/maxflow/Main.java
+++ src/main/java/maxflow/Main.java
@@ -3,7 +3,7 @@ package maxflow;
 public class Main {
     public static void main(String[] args) {
         try {
-            Graph graph = Parser.parseFile("src/main/resources/benchmark1.txt");
+            Graph graph = Parser.parseFile("/Users/leyanadaba/IdeaProjects/MaxFlowNetwork/src/main/resources/benchmark1.txt");
             MaxFlowSolver solver = new MaxFlowSolver(graph, 0, graph.V() - 1);
             System.out.println("Max flow: " + solver.getMaxFlow());
         } catch (Exception e) {
